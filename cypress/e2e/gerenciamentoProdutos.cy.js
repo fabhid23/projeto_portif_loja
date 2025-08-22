22548c14a6a35d748dd67723f6a4c08f39993544
fabhid23 <fab.takahara94@gmail.com>
2025-08-21 23:41:47 -0300

    adicionando teste para editar produto

--- cypress/e2e/gerenciamentoProdutos.cy.js
+++ cypress/e2e/gerenciamentoProdutos.cy.js
@@ -3,19 +3,29 @@ describe('Gerenciamento de Produtos', () => {
         cy.visit('/')
         cy.fazerLoginComCredenciaisValidas()
     })
-    afterEach(() => {
-        cy.removerProduto('Coca cola')
-    })
     
     it('Deve adicionar um novo produto', () => {
         cy.adcionarNovaCategoria('Bebida')
         cy.adicionarNovoProduto('Coca cola', 'Refrigerante', '5.6', '15', 'Bebida')
         cy.contains('.toast', 'Produto adicionado com sucesso!').should('be.visible')
+        cy.contains('tr', 'Coca cola').should('contain', 'Refrigerante')
+        cy.contains('tr', 'Coca cola').should('contain', '5.6')
+        cy.contains('tr', 'Coca cola').should('contain', '15')
+        cy.contains('tr', 'Coca cola').should('contain', 'Bebida')
     })
-    it.only('Deve remover um produto', () => {
+    it('Deve remover um produto', () => {
         cy.adcionarNovaCategoria('Grão')
-        cy.adicionarNovoProduto('Arroz', 'grao curto', '30,4', '20', 'Grão')
+        cy.adicionarNovoProduto('Arroz', 'grao curto', '30.4', '20', 'Grão')
         cy.removerProduto('Arroz')
         cy.contains('.toast', 'Produto excluído com sucesso!').should('be.visible')
     })
+    it('Deve editar um produto', () => {
+        cy.adcionarNovaCategoria('Grão')
+        cy.adicionarNovoProduto('Arroz', 'grao curto', '30,4', '20', 'Grão')
+        cy.editarProduto('Arroz', 'grao curto', '40.60', '30', 'Grão')
+        cy.contains('.toast', 'Produto atualizado com sucesso!').should('be.visible')
+        cy.contains('tr', 'Arroz').should('contain', '40.60')
+        cy.contains('tr', 'Arroz').should('contain', '30')
+    })
+
 })
--- cypress/support/commands/common.js
+++ cypress/support/commands/common.js
@@ -19,3 +19,17 @@ Cypress.Commands.add('removerProduto', (nome) => {
         }
     });
 });
+
+Cypress.Commands.add('editarProduto', (nome, descricao, preco, quantidade, categoria) => {
+    cy.produtoExiste(nome).then((existe) => {
+        if (existe) {
+            cy.get('table').contains('tr', nome).find('button').first().click();
+            cy.get('#productDescription').clear().type(descricao);
+            cy.get('#productPrice').clear().type(preco);
+            cy.get('#productQuantity').clear().type(quantidade);
+            cy.get('[data-cy="product-category-select"]').parent().find('.select-dropdown.dropdown-trigger').first().click()
+            cy.get(`.dropdown-content li:contains("${categoria}")`).first().should('be.visible').click()
+            cy.get('[data-cy="save-product-button"]').click()
+        }
+    })
+})
